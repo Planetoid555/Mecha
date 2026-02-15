25993d2470a20946b239c93e0c1e1cbb5e7ce3ca
Planetoid555 <52900860+Planetoid555@users.noreply.github.com>
2026-02-15 01:06:53 -0500

    Pilot Movement
    Pilot Controller Script Beta, Camera works and so does the movement

---
+++ Mecha/Assets/Scripts/PilotControl.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class PilotControl : MonoBehaviour
+{
+    public float xSens;
+    public float ySens;
+    public Transform pilotCamera;
+
+    [SerializeField] float pilotSpeed;
+
+    private Transform transPilot;
+    private Rigidbody rigidPilot;
+
+    float xRotate;
+    float yRotate;
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        transPilot = GetComponent<Transform>();
+        rigidPilot = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        //
+        float xMouse = Input.GetAxisRaw("Mouse X") * Time.deltaTime * xSens;
+        float yMouse = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * ySens;
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        yRotate += xMouse;
+        xRotate -= yMouse;
+        xRotate = Mathf.Clamp(xRotate, -90f, 90f);
+        pilotCamera.rotation = Quaternion.Euler(xRotate, yRotate, 0);
+        transPilot.rotation = Quaternion.Euler(0, yRotate, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        MovePilot();
+    }
+
+    private void MovePilot()
+    {
+        moveDirection = transPilot.forward * verticalInput + transPilot.right * horizontalInput;
+        rigidPilot.AddForce(moveDirection.normalized * pilotSpeed, ForceMode.Force);
+    }
+}
